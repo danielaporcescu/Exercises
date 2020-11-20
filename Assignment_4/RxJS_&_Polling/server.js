72840a2d792038dcb9d3dc8b9eceb89844fe76b0
Daniela Porcescu <32160331+danielaporcescu@users.noreply.github.com>
2020-11-20 11:30:07 +0100

    index hml scheletone

--- Assignment_4/RxJS_&_Polling/server.js
+++ Assignment_4/RxJS_&_Polling/server.js
@@ -3,4 +3,4 @@ var app = express();
 app.use(express.static(__dirname + '/src')); //__dir and not _dir
 var port = 8002; 
 app.listen(port);
-console.log('server on' + port);
+console.log('server on ' + port);
