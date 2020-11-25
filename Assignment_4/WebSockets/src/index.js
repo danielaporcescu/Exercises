364b4b1d2c750b4b2fc3b699d4804e5fa1cfeeaa
Daniela Porcescu <32160331+danielaporcescu@users.noreply.github.com>
2020-11-25 11:49:24 +0100

    almost done

--- Assignment_4/WebSockets/src/index.js
+++ Assignment_4/WebSockets/src/index.js
@@ -29,35 +29,32 @@ window.subscribeToWarnings = () => {
 };
 
 //GET WARNINGS SINCE TIME
-document.getElementById("myLocalDate").defaultValue = "2020-11-20T11:00:00.000";
+document.getElementById("myLocalDate").defaultValue = "2019-11-25T13:00:00.000";
 function getWarningsSince() {
-  // if (document.getElementById("onoff") != null) {
-  //   var p = document.getElementById("onoff");
-  //   p.remove();
-  // }
+  if (document.getElementById("onoff") != null) {
+    var p = document.getElementById("onoff");
+    p.remove();
+  }
   $.get(warningsSinceUrl + getFromTime(), (data) => {
     warningsSince = data;
-    var tr = document.createElement("tr");
-    tr.setAttribute("id", "onoff");
-    var warns = JSON.stringify(data.warnings);
-    // let el = document.createElement("p");
-    // el.setAttribute("id", "onoff");
-    // el.innerText = JSON.stringify(data.warnings);
-    // document.getElementById("warningSince").appendChild(el);
-    if (warns.prediction != null) {
-    tr.innerHTML =  
-    ` <td>${warns.id}</td>
-        <td>${warns.severity}</td>
-        <td>${warns.prediction.from}</td>
-        <td>${warns.prediction.to}</td>
-        <td>${warns.prediction.type}</td>
-        <td>${warns.prediction.unit}</td>
-        <td>${warns.prediction.time}</td>
-        <td>${warns.prediction.place}</td>
-        <td>${warns.prediction.precipitation_types || ""}</td>`;
 
-      document.getElementById("warningSince").appendChild(tr);
-  }});
+    document.getElementById("warningSince").innerHTML = warningsSince.warnings
+      .map(
+        (x) =>
+          `<tr>
+      <td>${x.id}</td>
+      <td>${x.severity}</td>
+      <td>${x.prediction.from}</td>
+      <td>${x.prediction.to}</td>
+      <td>${x.prediction.type}</td>
+      <td>${x.prediction.unit}</td>
+      <td>${x.prediction.time}</td>
+      <td>${x.prediction.place}</td>
+      <td>${x.prediction.precipitation_types || ""}</td>
+      </tr>`
+      )
+      .join("");
+  });
 }
 
 //TOGGLE BUTTON ONN/OFF to unsubscribe
@@ -78,18 +75,20 @@ function changeSeverity() {
   } else {
     unSubscribeToWarnings();
     var warningsTable = document.getElementById("warningsTable");
+    // console.log(warningsTable);
     while (warningsTable.childNodes.length != 2) {
       warningsTable.removeChild(warningsTable.childNodes[2]);
     }
-    console.log(warningsTable.childNodes);
+    console.log(warningData);
 
     for (let i = 0; i < warningData.length - 1; i++) {
-      if (warningData[i].prediction != null &&
-        warningData[i].severity == severity) {
+      if (
+        warningData[i].prediction != null &&
+        warningData[i].severity == severity
+      ) {
+        // console.log(warningData[i].severity);
+        // console.log(severity);
 
-        console.log(warningData[i].severity);
-        console.log(severity);
-        
         var tr = document.createElement("tr");
         tr.setAttribute("id", warningData[i].id);
 
@@ -111,7 +110,7 @@ function changeSeverity() {
 
 //PRINT THE TABLE
 function printToTable(warns) {
-  console.log(warns);
+  //console.log(warns);
   if (warns.time != null) {
     for (let warn of warns.warnings) {
       printWarnings(warn);
@@ -124,13 +123,15 @@ function printToTable(warns) {
 //PRINT TO HTML
 function printWarnings(warns) {
   if (warns != null) {
-    warningData.push(warns);
-
+    // warningData.push(warns);
+    // console.log(warningData)
+    var id = document.getElementById(warns.id);
+    if (id != null) id.remove();
     var tr = document.createElement("tr");
     tr.setAttribute("id", warns.id);
 
     if (warns.prediction != null) {
-      tr.innerHTML = ` <td>${warns.id}</td>
+      tr.innerHTML = `<td>${warns.id}</td>
         <td>${warns.severity}</td>
         <td>${warns.prediction.from}</td>
         <td>${warns.prediction.to}</td>
